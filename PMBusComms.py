4cefe799510134d38b80033d62c5f6f03287fa46
Angelika Kosciolek <andzelikakosciolek@gmail.com>
2017-03-19 18:23:12 +0000

    Update and rename pmbus.py to PMBusComms.py

--- PMBusComms.py
+++ PMBusComms.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python
+#####################################################################
+#                                                                   #
+#                           PMBusComms.py                           #
+#                     Author: Angelika Kosciolek                    #
+#                             05/03/2017                            #
+#                                                                   #
+#              Description: Set up PMBus communication              #
+#                                                                   #
+#####################################################################
 
-# import the PyUSB module
 import ftd2xx
 import time
-import Linear_Conversions
+import LinearConversions
 import re
 
 
-class PMBUS_COMMS:
+class PMBusComms:
 
     def set_up_device(self):
 
@@ -28,13 +35,14 @@ class PMBUS_COMMS:
         # set event notification
         # device.setEventNotification(ftd2xx.ftd2xx.EVENT_RXCHAR)
 
-    def configuration_command(self, command):
+    def write_command(self, device, command):
 
-        self.device.write(command + "\r\n")
+        time.sleep(0.005)
+        device.write(command + "\r\n")
         time.sleep(0.005)  # allow 5ms delay between the commands
-        self.device.read(self.device.getQueueStatus())
+        device.read(device.getQueueStatus())
 
-    def process(self):
+    def __init__(self):
 
         # list devices by description, returns tuple of attached devices description strings
         d = ftd2xx.listDevices()
@@ -45,16 +53,16 @@ class PMBUS_COMMS:
             print self.device
         except ftd2xx.ftd2xx.DeviceError:
             print "Unable to open 'EM2130 device'."
-            exit(1)
 
         self.set_up_device()
 
-        self.configuration_command("t11001")  # configure trigger time
-        self.configuration_command("t_020")  # configure trigger timeout
-        self.configuration_command("is_07")  # i2c speed 400kHz
-        self.configuration_command("ip_0")  # control pin off (device turned off)
+        self.write_command(self.device, "t11001")  # configure trigger time
+        self.write_command(self.device, "t_020")  # configure trigger timeout
+        self.write_command(self.device, "is_07")  # i2c speed 400kHz
+        self.write_command(self.device, "ip_0")  # control pin off (device turned off)
 
         # read version
+        time.sleep(0.005)
         self.device.write("v\r\n")
         time.sleep(0.005)
 
@@ -62,7 +70,7 @@ class PMBUS_COMMS:
             i = self.device.read(self.device.getQueueStatus())
             print "version = " + i[1:]
 
-        return self.device
+        #return self.device
 
         # send query
         #x = self.vout_query_command(self.device, "iq_3f01028b")  # READ_VOUT
@@ -72,6 +80,7 @@ class PMBUS_COMMS:
 
     def vout_query_command(self, device,  command):
 
+        time.sleep(0.005)
         device.write(command + "\r\n")  # 3f is my slave address
         time.sleep(0.005)
         response = ""
@@ -79,7 +88,7 @@ class PMBUS_COMMS:
             response = device.read(device.getQueueStatus())
         res = response.rstrip()
 
-        print "PMBus Response = 0x" + res[1:]
+        #print "PMBus Response = 0x" + res[1:]
         a = response[-4:]
         b = response[1:3]
         a = a.rstrip()
@@ -87,26 +96,26 @@ class PMBUS_COMMS:
         if (re.match("([a-f][0-9]|[0-9][a-f]|[0-9][0-9]|[a-f][a-f])", a) and
                 re.match("([a-f][0-9]|[0-9][a-f]|[0-9][0-9]|[a-f][a-f])", b)):
             r = int("0x" + a + b, 0)
-            print "Block command = 0x" + a + b
-            x = Linear_Conversions.Linear_Convertions()
-            result = x.L16_to_float(r)
-            print "Result = " + str(result) + " V\n"
+            #print "Block command = 0x" + a + b
+            x = LinearConversions.LinearConversions()
+            result = x.l16_to_float(r)
+            #print "Result = " + str(result) + " V\n"
             val = ("%.2f" % result)
             return val
         else:
             print("ERROR - Unvalid response\n")
-            return "Error"
-
+            return "0.0"
 
     def vin_query_command(self, device, command):
 
+        time.sleep(0.005)
         device.write(command + "\r\n")  # 3f is my slave address
         time.sleep(0.005)
         response = ""
         if device.getQueueStatus() > 0:
             response = device.read(device.getQueueStatus())
         res = response.rstrip()
-        print "PMBus Response = 0x" + res[1:]
+        #print "PMBus Response = 0x" + res[1:]
         a = response[-4:]
         b = response[1:3]
         a = a.rstrip()
@@ -114,21 +123,13 @@ class PMBUS_COMMS:
         if (re.match("([a-f][0-9]|[0-9][a-f]|[0-9][0-9]|[a-f][a-f])", a) and
                 re.match("([a-f][0-9]|[0-9][a-f]|[0-9][0-9]|[a-f][a-f])", b)):
             r = int("0x" + a + b, 0)
-            print "Block command = 0x" + a + b
-            x = Linear_Conversions.Linear_Convertions()
-            result = x.L11_to_float(r)
-            print "Result = %.2f" % result + " V\n"
+            #print "Block command = 0x" + a + b
+            x = LinearConversions.LinearConversions()
+            result = x.l11_to_float(r)
+            #print "Result = %.2f" % result + " V\n"
             val = ("%.2f" % result)
             return val
         else:
             print("ERROR - Unvalid response\n")
-            return "Error"
+            return "0.0"
 
-
-def main():
-    s = PMBUS_COMMS()
-    s.process()
-    print('\nDone!')
-
-if __name__ == '__main__':
-    main()

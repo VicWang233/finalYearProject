14f9ea4aec6491c0074c960b65d1bd3c91277067
Angelika Kosciolek <andzelikakosciolek@gmail.com>
2017-04-14 11:51:07 +0100

    Update PMBusComms.py

--- PMBusComms.py
+++ PMBusComms.py
@@ -16,32 +16,6 @@ import re
 
 class PMBusComms:
 
-    def set_up_device(self):
-
-        # set baud rate
-        self.device.setBaudRate(1 * 1000 * 1000)
-        # set data characteristics
-        self.device.setDataCharacteristics(ftd2xx.ftd2xx.BITS_8, ftd2xx.ftd2xx.STOP_BITS_1, ftd2xx.ftd2xx.PARITY_NONE)
-        # purge
-        self.device.purge()
-        # set RX/TX timeouts
-        self.device.setTimeouts(300, 300)
-        # set bit mode
-        self.device.setBitMode(0xFF, 0x40)
-        # set flow control
-        self.device.setFlowControl(ftd2xx.ftd2xx.FLOW_NONE, 0, 0)
-        # set chars
-        self.device.setChars(0x0A, 0x7F, 0, 0)
-        # set event notification
-        # device.setEventNotification(ftd2xx.ftd2xx.EVENT_RXCHAR)
-
-    def write_command(self, device, command):
-
-        time.sleep(0.005)
-        device.write(command + "\r\n")
-        time.sleep(0.005)  # allow 5ms delay between the commands
-        device.read(device.getQueueStatus())
-
     def __init__(self):
 
         # list devices by description, returns tuple of attached devices description strings
@@ -56,10 +30,10 @@ class PMBusComms:
 
         self.set_up_device()
 
-        self.write_command(self.device, "t11001")  # configure trigger time
-        self.write_command(self.device, "t_020")  # configure trigger timeout
-        self.write_command(self.device, "is_07")  # i2c speed 400kHz
-        self.write_command(self.device, "ip_0")  # control pin off (device turned off)
+        self.configure_command(self.device, "t11001")  # configure trigger time
+        self.configure_command(self.device, "t_020")  # configure trigger timeout
+        self.configure_command(self.device, "is_07")  # i2c speed 400kHz
+        self.configure_command(self.device, "ip_0")  # control pin off (device turned off)
 
         # read version
         time.sleep(0.005)
@@ -78,7 +52,32 @@ class PMBusComms:
         #y = self.vin_query_command(self.device, "iq_3f010288")  # READ_VIN
         #print y
 
-    def vout_query_command(self, device,  command):
+    def set_up_device(self):
+
+        # set baud rate
+        self.device.setBaudRate(1 * 1000 * 1000)
+        # set data characteristics
+        self.device.setDataCharacteristics(ftd2xx.ftd2xx.BITS_8, ftd2xx.ftd2xx.STOP_BITS_1, ftd2xx.ftd2xx.PARITY_NONE)
+        # purge
+        self.device.purge()
+        # set RX/TX timeouts
+        self.device.setTimeouts(300, 300)
+        # set bit mode
+        self.device.setBitMode(0xFF, 0x40)
+        # set flow control
+        self.device.setFlowControl(ftd2xx.ftd2xx.FLOW_NONE, 0, 0)
+        # set chars
+        self.device.setChars(0x0A, 0x7F, 0, 0)
+
+    def configure_command(self, device, command):
+
+        time.sleep(0.005)
+        device.write(command + "\r\n")
+        time.sleep(0.005)  # allow 5ms delay between the commands
+        read = device.read(device.getQueueStatus())
+        return read
+
+    def l16_query_command(self, device,  command):
 
         time.sleep(0.005)
         device.write(command + "\r\n")  # 3f is my slave address
@@ -103,10 +102,33 @@ class PMBusComms:
             val = ("%.2f" % result)
             return val
         else:
-            print("ERROR - Unvalid response\n")
-            return "0.0"
+            time.sleep(0.005)
+            device.write(command + "\r\n")  # 3f is my slave address
+            time.sleep(0.005)
+            response = ""
+            if device.getQueueStatus() > 0:
+                response = device.read(device.getQueueStatus())
+            res = response.rstrip()
 
-    def vin_query_command(self, device, command):
+            # print "PMBus Response = 0x" + res[1:]
+            a = response[-4:]
+            b = response[1:3]
+            a = a.rstrip()
+            b = b.rstrip()
+            if (re.match("([a-f][0-9]|[0-9][a-f]|[0-9][0-9]|[a-f][a-f])", a) and
+                    re.match("([a-f][0-9]|[0-9][a-f]|[0-9][0-9]|[a-f][a-f])", b)):
+                r = int("0x" + a + b, 0)
+                # print "Block command = 0x" + a + b
+                x = LinearConversions.LinearConversions()
+                result = x.l16_to_float(r)
+                # print "Result = " + str(result) + " V\n"
+                val = ("%.2f" % result)
+                return val
+            else:
+                print("ERROR - Unvalid response\n")
+                return "0.0"
+
+    def l11_query_command(self, device, command):
 
         time.sleep(0.005)
         device.write(command + "\r\n")  # 3f is my slave address
@@ -130,6 +152,70 @@ class PMBusComms:
             val = ("%.2f" % result)
             return val
         else:
-            print("ERROR - Unvalid response\n")
-            return "0.0"
+            time.sleep(0.005)
+            device.write(command + "\r\n")  # 3f is my slave address
+            time.sleep(0.005)
+            response = ""
+            if device.getQueueStatus() > 0:
+                response = device.read(device.getQueueStatus())
+            res = response.rstrip()
+            # print "PMBus Response = 0x" + res[1:]
+            a = response[-4:]
+            b = response[1:3]
+            a = a.rstrip()
+            b = b.rstrip()
+            if (re.match("([a-f][0-9]|[0-9][a-f]|[0-9][0-9]|[a-f][a-f])", a) and
+                    re.match("([a-f][0-9]|[0-9][a-f]|[0-9][0-9]|[a-f][a-f])", b)):
+                r = int("0x" + a + b, 0)
+                # print "Block command = 0x" + a + b
+                x = LinearConversions.LinearConversions()
+                result = x.l11_to_float(r)
+                # print "Result = %.2f" % result + " V\n"
+                val = ("%.2f" % result)
+                return val
+            else:
+                print("ERROR - Unvalid response\n")
+                return "0.0"
+
+    def l11_write_command(self, device, command, value):
+
+        x = LinearConversions.LinearConversions()
+        result = x.float_to_l11(value)
+        a = result[-2:]
+        b = result[2:4]
+        a = a.rstrip()
+        b = b.rstrip()
+        val = a + b
+        time.sleep(0.005)
+        device.write(command + val + "\r\n")
+        time.sleep(0.005)  # allow 5ms delay between the commands
+        device.read(device.getQueueStatus())
+
+    def l16_write_command(self, device, command, value):
+
+        x = LinearConversions.LinearConversions()
+        result = x.float_to_l16(value)
+        a = result[-2:]
+        b = result[2:4]
+        a = a.rstrip()
+        b = b.rstrip()
+        val = a + b
+        time.sleep(0.005)
+        device.write(command + val + "\r\n")
+        time.sleep(0.005)  # allow 5ms delay between the commands
+        device.read(device.getQueueStatus())
+
+    def write_direct(self, device, command, hex_val):
+
+        result = hex_val
+        a = result[-2:]
+        b = result[2:4]
+        val = a + b
+        cmd = str(command) + str(val) + "\r\n"
+        time.sleep(0.005)
+        device.write(cmd)
+        time.sleep(0.005)  # allow 5ms delay between the commands
+        device.read(device.getQueueStatus())
+        print result
+
 
